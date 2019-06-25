8baefc620044fb5ec54df80335f269e9e9053671
Stephen Lau <stephen@v1040-wn-rt-c-125-153.campus-dynamic.uwaterloo.ca>
2019-06-24 21:30:32 -0400

    fixed bug

--- app/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierSkinDetNet.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierSkinDetNet.java
@@ -57,7 +57,7 @@ public class ClassifierSkinDetNet extends Classifier {
         // you can download this file from
         // see build.gradle for where to obtain this file. It should be auto
         // downloaded into assets.
-        return "";
+        return "skindetnet.tflite";
     }
 
     @Override
