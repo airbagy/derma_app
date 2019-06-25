866a0bf416234592efdfd20411b8ad76c731125f
Omar Qadri <o3qadri@uwaterloo.ca>
2019-06-25 17:31:39 -0400

    Fix index bounds exception in some cases

--- app/src/main/java/org/tensorflow/lite/examples/classification/CameraActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/CameraActivity.java
@@ -379,7 +379,7 @@ public abstract class CameraActivity extends AppCompatActivity
     if (requestCode == PERMISSIONS_REQUEST) {
       if (grantResults.length > 0
           && grantResults[0] == PackageManager.PERMISSION_GRANTED
-          && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+          && (grantResults.length == 1 || grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
         setFragment();
       } else {
         requestPermission();
