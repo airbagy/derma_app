a1070e807ba0027e7b7f91be7749e92d87555b20
Stephen Lau <stephen@v1040-wn-rt-c-125-153.campus-dynamic.uwaterloo.ca>
2019-06-25 18:13:28 -0400

    initial screens

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -0,0 +1,16 @@
+package org.tensorflow.lite.examples.classification;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_message_android);
+    }
+
+}
