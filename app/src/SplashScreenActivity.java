fdca3fde91f9356a43f5b854804da8fd4fbee6ba
Stephen Lau <stephen@v1040-wn-rt-c-125-153.campus-dynamic.uwaterloo.ca>
2019-06-25 17:25:36 -0400

    initial splash pages

---
+++ app/src/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+
+package viralandroid.com.androidsplashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_layout);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
