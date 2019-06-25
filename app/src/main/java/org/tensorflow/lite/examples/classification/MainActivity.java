69844b1dcdc833fe528a4448cc65725ce7af51cb
Omar Qadri <o3qadri@uwaterloo.ca>
2019-06-25 19:04:28 -0400

    make welcome screen lead to classifier screen

--- app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -2,7 +2,9 @@ package org.tensorflow.lite.examples.classification;
 
 import android.app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +13,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_message_android);
+    }
+
+    public void dismissWelcomeMessageBox(View v) {
+        Intent intent = new Intent(this, ClassifierActivity.class);
+        startActivity(intent);
     }
 
 }
