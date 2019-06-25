ce7b9687a356edf484815812fec83d51a95867f9
Stephen Lau <stephen@v1040-wn-rt-c-125-153.campus-dynamic.uwaterloo.ca>
2019-06-25 14:34:09 -0400

    adding splash screen page

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -0,0 +1,14 @@
+package org.tensorflow.lite.examples.classification;
+
+public class MainActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Make sure this is before calling super.onCreate
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+    }
+
+}
