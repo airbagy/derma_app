a46de73d5d15515d2ed6d483631ae74ef592de67
unknown <lishid316@gmail.com>
2019-07-10 09:07:23 -0400

    implement picture fetch

--- app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -5,7 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.net.Uri;
+import android
 
 public class MainActivity extends Activity {
 
@@ -28,7 +29,34 @@ public class MainActivity extends Activity {
     public void accessLibrary(View v){
         Intent intent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        String[] mimetypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
         startActivityForResult(intent, 0);
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    //data.getData return the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { android.provider.MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+                    //Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    //Gets the String value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+                    // Set the Image in ImageView after decoding the String
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                    break;
+
+            }
     }
 
     public void activateInfoPage(View v){
