52ab8d7819f735311a7f6af3e1d849fe9f8ffb6f
Omar Qadri <o3qadri@uwaterloo.ca>
2019-07-10 18:44:07 -0400

    add to sharing activity

--- app/src/main/java/org/tensorflow/lite/examples/classification/SharingActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/SharingActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Properties;
 import javax.mail.*;
+import javax.crypto.*;
 
 
 public class SharingActivity extends Activity {
@@ -25,7 +28,9 @@ public class SharingActivity extends Activity {
         String message = ((EditText)findViewById(R.id.send_message)).getText().toString();
         String code = ((EditText)findViewById(R.id.send_code)).getText().toString();
 
-        sendEmail(address, subject, message);
+        String encodedMessage = encodeString(message, code);
+
+        sendEmail(address, subject, encodedMessage);
 
         TextView result = (TextView)findViewById(R.id.send_result);
         result.setText("Sent " + subject);
@@ -38,13 +43,24 @@ public class SharingActivity extends Activity {
         String subject = ((EditText)findViewById(R.id.recv_subject)).getText().toString();
         String code = ((EditText)findViewById(R.id.recv_code)).getText().toString();
 
-        recvEmail(address, password, subject);
+        String status = "";
+        try {
+            String message = recvEmail(address, password, subject);
+            if (message != null) {
+                String decodedMessage = decodeString(message, code);
+                status = "Received " + subject + ": '" + decodedMessage + "'";
+            } else {
+                status = "No message for subject " + subject;
+            }
+        } catch (Exception e) {
+            status = "Error: " + e.getMessage();
+        }
 
         TextView result = (TextView)findViewById(R.id.recv_result);
-        result.setText("Received " + subject);
+        result.setText(status);
     }
 
-    public void sendEmail(String address, String subject, String message) {
+    private void sendEmail(String address, String subject, String message) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.putExtra(Intent.EXTRA_EMAIL, new String[]{ address });
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
@@ -57,8 +73,63 @@ public class SharingActivity extends Activity {
         startActivity(Intent.createChooser(intent, "Choose an email client:"));
     }
 
-    public void recvEmail(String address, String password, String subject) {
-        return;
+    private String encodeString(String str, String code) {
+        // add encryption as well
+        return encodeString64(str);
+    }
+
+    private String decodeString(String str, String code) {
+        // add decryption as well
+        return decodeString64(str);
+    }
+
+    private String encodeString64(String str) {
+        try {
+            Base64.encodeToString(str.getBytes("UTF-8"), Base64.DEFAULT);
+        } catch (Exception e) {}
+        return null;
+    }
+
+    private String decodeString64(String str) {
+        try {
+            return new String(Base64.decode(str, Base64.DEFAULT), "UTF-8");
+        } catch (Exception e) {}
+        return null;
+    }
+
+    private String recvEmail(String address, String password, String subject) throws Exception {
+        String host = "pop.gmail.com";
+        Properties properties = new Properties();
+
+        properties.put("mail.pop3.host", host);
+        properties.put("mail.pop3.port", "995");
+        properties.put("mail.pop3.starttls.enable", "true");
+        Session emailSession = Session.getDefaultInstance(properties);
+
+        Store store = emailSession.getStore("pop3s");
+
+        store.connect(host, address, password);
+        Folder emailFolder = store.getFolder("INBOX");
+        emailFolder.open(Folder.READ_ONLY);
+
+        Message[] messages = emailFolder.getMessages();
+        String message = recvMessage(messages, 10, subject);
+
+        emailFolder.close(false);
+        store.close();
+
+        return message;
+    }
+
+    private String recvMessage(Message[] messages, int maxLen, String subject) throws Exception {
+        int len = Math.min(maxLen, messages.length);
+        for (int i = 0; i < len; i++) {
+            Message message = messages[i];
+            if (message.getSubject().equals(subject)) {
+                return message.getContent().toString();
+            }
+        }
+        return null;
     }
 
 }
