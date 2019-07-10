7cfefa5920d9843a369e9468da6326f23eeb6410
Li Shi Wang <lishid316@gmail.com>
2019-07-10 19:41:24 -0400

    Implement image select classification

--- app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -7,9 +7,82 @@ import android.os.Bundle;
 import android.view.View;
 import android.net.Uri;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import android.graphics.Bitmap;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+
+import org.tensorflow.lite.examples.classification.tflite.Classifier;
+
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Typeface;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Size;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Toast;
+import java.io.IOException;
+import java.util.List;
+import org.tensorflow.lite.examples.classification.env.BorderedText;
+import org.tensorflow.lite.examples.classification.env.ImageUtils;
+import org.tensorflow.lite.examples.classification.env.Logger;
+import org.tensorflow.lite.examples.classification.tflite.Classifier;
+import org.tensorflow.lite.examples.classification.tflite.Classifier.Device;
+import org.tensorflow.lite.examples.classification.tflite.Classifier.Model;
+
 public class MainActivity extends Activity {
 
     public static final int GALLERY_REQUEST_CODE = 0;
+    public static final int CAMERA_REQUEST_CODE = 1;
+    private static final Logger LOGGER = new Logger();
+    private Classifier classifier;
+    private Model model = Model.QUANTIZED;
+    private Device device = Device.CPU;
+    private int numThreads = -1;
+
+    protected void processImage(Bitmap image) {
+        recreateClassifier(model, device, numThreads);
+        Bitmap croppedBitmap = Bitmap.createScaledBitmap(image,
+                classifier.getImageSizeX(), classifier.getImageSizeY(), false);
+        System.out.println("ProcessImage");
+        if (classifier != null) {
+            final long startTime = SystemClock.uptimeMillis();
+            final List<Classifier.Recognition> results = classifier.recognizeImage(croppedBitmap);
+            System.out.println(results);
+        }
+    }
+
+    private void recreateClassifier(Model model, Device device, int numThreads) {
+        if (classifier != null) {
+            LOGGER.d("Closing classifier.");
+            classifier.close();
+            classifier = null;
+        }
+        if (device == Device.GPU && model == Model.QUANTIZED) {
+            LOGGER.d("Not creating classifier: GPU doesn't support quantized models.");
+            runOnUiThread(
+                    () -> {
+                        Toast.makeText(this, "GPU does not yet supported quantized models.", Toast.LENGTH_LONG)
+                                .show();
+                    });
+            return;
+        }
+        try {
+            LOGGER.d(
+                    "Creating classifier (model=%s, device=%s, numThreads=%d)", model, device, numThreads);
+            classifier = Classifier.create(this, model, device, numThreads);
+        } catch (IOException e) {
+            LOGGER.e(e, "Failed to create classifier.");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +110,48 @@ public class MainActivity extends Activity {
         startActivityForResult(intent.createChooser(intent, "Select your image sample."), GALLERY_REQUEST_CODE);
     }
 
+    private void processCameraImage(View v) {
+        Intent cameraIntent = new
+                Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case GALLERY_REQUEST_CODE:
+                    System.out.println("gallery");
+                    System.out.println(data.getData());
+                    if (data.getData() != null) {
+                        Uri imageURI = data.getData();
+                        URI imageJURI = URI.create(imageURI.toString());
+                        System.out.println(imageURI);
+                        try{
+                            Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageURI);
+                            System.out.println(imageBitmap.getWidth());
+                            System.out.println(imageBitmap.getHeight());
+                            //Intent intent = new Intent(this, ImageClassifierActivity.class);
+                            //intent.putExtra("bitmap", imageBitmap);
+                           // startActivity(intent);
+                            processImage(imageBitmap);
+                        }
+                        catch (IOException e) {
+                            System.out.println("Cannot process image");
+                        }
+                    }
+                    break;
+
+                case CAMERA_REQUEST_CODE:
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+
+                    //Starting activity (ImageViewActivity in my code) to preview image
+                    Intent intent = new Intent(this, ImageClassifierActivity.class);
+                    intent.putExtra("bitmap", photo);
+                    startActivity(intent);
+            }
+        }
+    }
+
     public void accessMaps(View v) {
         // Create a Uri from an intent string. Use the result to create an Intent.
         Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
@@ -48,23 +163,6 @@ public class MainActivity extends Activity {
 
 // Attempt to start an activity that can handle the Intent
         startActivity(mapIntent);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (resultCode == Activity.RESULT_OK) {
-            switch (requestCode) {
-                case GALLERY_REQUEST_CODE:
-                    if (data.getData() != null) {
-                        Uri imageURI = data.getData();
-
-                        Intent intent = new Intent(this, ClassifierActivity.class);
-                        intent.putExtra("imageURI", imageURI.toString());
-                        startActivity(intent);
-                    }
-                    break;
-
-            }
-        }
     }
 
     public void activateInfoPage(View v){
