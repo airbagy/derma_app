101323c8468780440a7457cc6af01a259ff40c90
unknown <lishid316@gmail.com>
2019-07-10 09:07:23 -0400

    implement picture fetch

--- app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -6,8 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.net.Uri;
+<<<<<<< HEAD
 
 
+=======
+import android
+>>>>>>> implement picture fetch
 
 public class MainActivity extends Activity {
 
@@ -30,9 +34,13 @@ public class MainActivity extends Activity {
     public void accessLibrary(View v){
         Intent intent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        String[] mimetypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
         startActivityForResult(intent, 0);
     }
 
+<<<<<<< HEAD
     public void accessMaps(View v) {
         // Create a Uri from an intent string. Use the result to create an Intent.
         Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
@@ -44,6 +52,30 @@ public class MainActivity extends Activity {
 
 // Attempt to start an activity that can handle the Intent
         startActivity(mapIntent);
+=======
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    //data.getData return the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { android.provider.MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+                    //Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    //Gets the String value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+                    // Set the Image in ImageView after decoding the String
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                    break;
+
+            }
+>>>>>>> implement picture fetch
     }
 
     public void activateInfoPage(View v){
