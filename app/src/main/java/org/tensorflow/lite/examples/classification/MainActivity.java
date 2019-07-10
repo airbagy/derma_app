c09d3a9a3c92f2e92b5f487482388c89209ee254
Stephen Lau <stephen@v1040-wn-rt-c-123-133.campus-dynamic.uwaterloo.ca>
2019-07-10 12:45:28 -0400

    maps functionality

--- app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.net.Uri;
+
 
 
 public class MainActivity extends Activity {
@@ -29,6 +31,19 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, 0);
+    }
+
+    public void accessMaps(View v) {
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
+
+// Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+// Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+// Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
     }
 
     public void activateInfoPage(View v){
