c87a0fe2fa98a86a9e43ceb8b636d7bee0af27f9
ssmlau <ssmlau@edu.uwaterloo.ca>
2019-07-19 13:59:22 -0400

    result model singleton

--- app/src/main/java/org/tensorflow/lite/examples/classification/SplashScreenActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/SplashScreenActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.tensorflow.lite.examples.classification.customview.AutoFitTextureView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -26,6 +31,33 @@ public class SplashScreenActivity extends AppCompatActivity {
                 SplashScreenActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    @Override
+    public View onCreateView(
+            final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        return inflater.inflate(layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
+        textureView = (AutoFitTextureView) view.findViewById(R.id.texture);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startBackgroundThread();
+        // When the screen is turned off and turned back on, the SurfaceTexture is already
+        // available, and "onSurfaceTextureAvailable" will not be called. In that case, we can open
+        // a camera and start preview from here (otherwise, we wait until the surface is ready in
+        // the SurfaceTextureListener).
+
+        if (textureView.isAvailable()) {
+            camera.startPreview();
+        } else {
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
     }
 
 }
