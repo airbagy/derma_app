cfb87996b6a9f5a6280ccc017c2d921c07a8294c
ssmlau <ssmlau@edu.uwaterloo.ca>
2019-07-20 17:36:53 -0400

    fixing all layouts

--- app/src/main/java/org/tensorflow/lite/examples/classification/SplashScreenActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/SplashScreenActivity.java
@@ -3,11 +3,12 @@ package org.tensorflow.lite.examples.classification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.tensorflow.lite.examples.classification.customview.AutoFitTextureView;
 
@@ -26,8 +27,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_layout);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
